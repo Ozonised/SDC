26ba7dc27fc0b12b25298b8132444fac54806316
Farhan Khan <farhanzoom471@gmail.com>
2023-10-19 15:17:07 +0530

    modified drive scanArea & added PID_init
    added pid control for driving forward and making turns
    
     Condensed scanArea()

--- SDC_Firmware/include/state.h
+++ SDC_Firmware/include/state.h
@@ -4,12 +4,12 @@
 #include "servo.h"
 #include "hcsr04.h"
 #include "l293d.h"
-
+#include "PID.h"
 
 extern HCSR04 hcsr04;
 extern Direction dir;
-extern volatile unsigned long measured_left_motor_count, measured_right_motor_count;
 extern void (*currentState)(void);
 
+void PID_Init();
 void scanArea();
 void drive();
